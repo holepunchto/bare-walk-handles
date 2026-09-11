82c86955cbd7a2c4824eaa8874b6c329e024fe59
Kasper Isager Dalsgarð <kasperisager@hey.com>
2026-09-11 12:25:29 +0200

    Add Lunte, Prettier, and publish workflow (#1)

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const binding = require('./binding')
 
-module.exports = exports = function * walkHandles () {
+module.exports = exports = function* walkHandles() {
   const handles = []
 
   binding.walkHandles((handle) => handles.push(new Handle(handle)))
 
-  yield * handles
+  yield* handles
 }
 
 exports.constants = binding.constants
 
 class Handle {
-  constructor (opts = {}) {
+  constructor(opts = {}) {
     this.type = opts.type
     this.address = opts.address
     this.isActive = opts.isActive !== 0
